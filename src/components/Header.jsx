0fe66ffd37b5eb26f89aff03b2a21c2ca2752cba
Pradeep106 <87457239+Pradeep106@users.noreply.github.com>
2024-07-18 22:35:38 +0530

    Implemented theme mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,39 @@
+import Link from "next/link";
 import React from "react";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ModeToggle } from "./Helper/ModeToggle";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Hello Header!</h1>
-    </div>
+    <header>
+      <nav className="flex justify-between items-center ">
+        <ul>
+          <li>
+            <Link href="/">Pradeep Yadav</Link>
+          </li>
+        </ul>
+        <ul className="center-flex-item gap-5">
+          <li>
+            <Link href="/">Skill</Link>
+          </li>
+          <li>
+            <Link href="/">Experience</Link>
+          </li>
+          <li>
+            <Link href="/">Project</Link>
+          </li>
+          <ModeToggle />
+        </ul>
+      </nav>
+    </header>
   );
 };
 
